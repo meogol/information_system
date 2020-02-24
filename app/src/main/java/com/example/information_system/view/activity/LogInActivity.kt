24cbb70ac7a3e5f73ac7009352fa31e22aa4c0c9
Phenecy <phenecy.dev@gmail.com>
2020-02-24 14:42:06 +0300

    feat(View): added dialog fragment nav

--- app/src/main/java/com/example/information_system/view/activity/LogInActivity.kt
+++ app/src/main/java/com/example/information_system/view/activity/LogInActivity.kt
@@ -2,8 +2,12 @@ package com.example.information_system.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.widget.Toolbar
 import com.example.information_system.R
+import com.example.information_system.view.fragments.dialogFragments.PasswordResetDialogFragment
+
+private lateinit var bForgot: Button
 
 class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +18,26 @@ class LogInActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayHomeAsUpEnabled(true);
         supportActionBar!!.setHomeButtonEnabled(true);
         supportActionBar!!.setDisplayShowTitleEnabled(false);
-        toolbar.title="Войти"
+        toolbar.title = "Войти"
+
+        initViews()
+        initClickers()
+    }
+
+    private fun initViews() {
+        bForgot = findViewById(R.id.forgot_btn)
+    }
+
+    private fun initClickers() {
+        bForgot.setOnClickListener {
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            val prev = supportFragmentManager.findFragmentByTag("reset_pass")
+            if (prev != null) {
+                fragmentTransaction.remove(prev)
+            }
+            fragmentTransaction.addToBackStack(null)
+            val dialogFragment = PasswordResetDialogFragment()
+            dialogFragment.show(fragmentTransaction, "reset_pass")
+        }
     }
 }
